1c52f7f950676aef1fee54e53beeefd4c94543c6
mario2s <mario.diankov@gmail.com>
2026-05-27 09:42:15 +0300

    fix(jest): add test environment and mock reset configurations

--- autoops-mobile/jest.config.js
+++ autoops-mobile/jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   preset: 'jest-expo',
   testMatch: ['<rootDir>/src/__tests__/**/*.test.ts'],
+  testEnvironment: 'node',
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|expo-modules-core|@react-navigation/.*|jwt-decode))',
   ],
+  resetMocks: false,
+  clearMocks: false,
 };
